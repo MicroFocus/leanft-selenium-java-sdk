532ec284b7fbaadec8815ffde1cb3d225b9689b1
Yevgeny Efter <YevgenyEfter@gmail.com>
2018-05-01 09:53:50 +0300

    Fixing a bug of taking a snapshot of an area element.

--- src/main/resources/snapshot.js
+++ src/main/resources/snapshot.js
@@ -71,14 +71,131 @@ function calculateScrollTopDistance(parentPane, row, direction) {
     return distance;
 }
 
+function getImageElementForMapElement (elem) {
+    var parent = elem;
+    while (parent) {
+        if (parent.tagName.toLowerCase() === "map") {
+            break;
+        }
+        parent = parent.parentNode;
+    }
+
+    if (parent) {
+        var name = parent.name;
+        for (var i = 0; i < document.images.length; i++) {
+            if (document.images[i].useMap.substring(1) === name) {
+                return document.images[i];
+            }
+        }
+    }
+}
+
+function calcRectForElement (elem) {
+    if (elem.tagName.toLowerCase() === "map") {
+        var imgElem = getImageElementForMapElement(elem);
+        var rect = {
+            left: 0,
+            top: 0,
+            right: 0,
+            bottom: 0
+        };
+
+        if (!imgElem) {
+            return rect;
+        }
+
+        return imgElem.getBoundingClientRect();
+    }
+    else if (elem.tagName.toLowerCase() === "area") {
+        var imgElem = getImageElementForMapElement(elem);
+        var rect = {
+            left: 0,
+            top: 0,
+            right: 0,
+            bottom: 0
+        };
+
+        if (!imgElem) {
+            return rect;
+        }
+
+        var img_rect = imgElem.getBoundingClientRect();
+        var coords = elem.coords.split(/\s*,\s*/);
+        coords = coords.map(function (e) {
+            return parseInt(e, 10);
+        });
+
+        switch (elem.shape.toLowerCase()) {
+            case "rect":
+                if (coords.length === 4) {
+                    rect.left = coords[0];
+                    rect.top = coords[1];
+                    rect.right = coords[2];
+                    rect.bottom = coords[3];
+                }
+                break;
+            case "circle":
+                if (coords.length === 3) {
+                    var x = coords[0];
+                    var y = coords[1];
+                    var radius = coords[2];
+                    if (radius < 0) {
+                        break;
+                    }
+
+                    rect.left = x - radius;
+                    rect.top = y - radius;
+                    rect.right = x + radius;
+                    rect.bottom = y + radius;
+                }
+                break;
+            case "poly":
+            case "polygon":
+                if (coords.length >= 2) {
+                    var x1, x2;
+                    var y1, y2;
+                    x1 = x2 = coords[0];
+                    y1 = y2 = coords[1];
+                    for (var i = 2; i < coords.length; i += 2) {
+                        x1 = x1 < coords[i] ? x1 : coords[i];
+                        x2 = x2 > coords[i] ? x2 : coords[i];
+                        y1 = y1 < coords[i + 1] ? y1 : coords[i + 1];
+                        y2 = y2 > coords[i + 1] ? y2 : coords[i + 1];
+                    }
+                    rect.left = x1;
+                    rect.top = y1;
+                    rect.right = x2;
+                    rect.bottom = y2;
+                }
+                break;
+        }
+
+        var calcRect = {
+            left: Math.round(img_rect.left + rect.left),
+            top: Math.round(img_rect.top + rect.top),
+            right: Math.round(img_rect.left + rect.right),
+            bottom: Math.round(img_rect.top + rect.bottom)
+        };
+
+        calcRect.width = calcRect.right - calcRect.left;
+        calcRect.height = calcRect.bottom - calcRect.top;
+        return calcRect;
+    }
+    else {
+        return elem.getBoundingClientRect();
+    }
+}
+
 return (function(obj) {
-        var isIE = false || !!document.documentMode;
+    var isIE = false || !!document.documentMode;
         if (isIE) {
             var direction = determineRowScrollDirection(obj.parentNode, obj);
             scrollRowIntoView(obj.parentNode, obj, direction);
         } else
             obj.scrollIntoView(true);
-        var rect = obj.getBoundingClientRect();
-        return {left: rect.left, top: rect.top, width: rect.width, height: rect.height};
+        var rect = calcRectForElement(obj);
+        return {
+            left: rect.left, top: rect.top, width: rect.right - rect.left, height: rect.bottom - rect.top
+        };
     }
 ).apply(undefined, arguments);
