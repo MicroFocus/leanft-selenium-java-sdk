e5387b748a81c4ae8bbecb74b79e96f23a47fbfb
Patricia Gaspar <patricialorena.gaspar@microfocus.com>
2021-11-10 13:37:27 +0200

    Update copyright to 2021

--- src/test/java/com/hpe/leanft/selenium/UtilsTest.java
+++ src/test/java/com/hpe/leanft/selenium/UtilsTest.java
@@ -1,4 +1,4 @@
-/*! (c) Copyright 2015 - 2018 Micro Focus or one of its affiliates. */
+/*! (c) Copyright 2015 – 2021 Micro Focus or one of its affiliates. */
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // You may not use this file except in compliance with the License.
